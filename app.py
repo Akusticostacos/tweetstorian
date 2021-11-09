f26996e3bf0d6fffc42e79f50eeeead5d5ef705d
Aku Pasanen <aku.s.a.pasanen@student.jyu.fi>
2021-11-09 03:21:41 +0200

    app.py kommentit, templates hahmottelua

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+# Sovelluksen etusivu
 @app.route("/")
-def index():
-    return "Test Test"
+def frontpage():
+    return render_template("frontpage.html")
+
+
+# Käynnistetään sovellus debug-modessa
+if __name__ == "__main__":
+    app.run(debug=True)
+
