ff4f71d75a0db48ac02142e42e6e373e8317e180
Aku Pasanen <aku.s.a.pasanen@student.jyu.fi>
2022-05-02 18:43:48 +0300

    admin näkymän jatkoa...

--- application/views/admin.py
+++ application/views/admin.py
@@ -1,9 +1,10 @@
 from flask import app, Flask, render_template, Blueprint
 import requests, json
+from application.control.trends import get_trends, add_trends, query_trends, show_all, get_all_entries, add_additional_info, query_additional_info
 
 admin_blueprint = Blueprint("admin", __name__)
 
 # Sovelluksen etusivu
-@admin_blueprint.route("/admin")
+@admin_blueprint.route("/admin", methods=("GET","POST"))
 def admin():
-    return render_template("admin.html")
+    return render_template("admin.html", all_dates=reversed(get_all_entries()))
